ee099bb6fe0171e843b792734550945dc118094b
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-05-04 15:12:08 +0400

    fetched data front end

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -1,6 +1,11 @@
 import React, { useContext, useReducer, ReactNode } from "react";
 import axios from "axios";
 import reducer from "./reducer";
+import {
+  FETCH_DATA_ERROR,
+  FETCH_DATA_REQUEST,
+  FETCH_DATA_SUCCESS,
+} from "./actions";
 
 const initialState = {
   loading: false,
@@ -11,7 +16,7 @@ const initialState = {
 const AppContext = React.createContext<any>(null);
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
-  const [state, setState] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const dataFetch = axios.create({
     baseURL: "/api/v1",
@@ -30,10 +35,29 @@ const AppProvider = ({ children }: { children: ReactNode }) => {
     }
   );
 
-  const fetchAllData = () => {};
+  const fetchAllData = async () => {
+    let url = `/data`;
+    dispatch({ type: FETCH_DATA_REQUEST });
+    try {
+      const { data } = await dataFetch.get(url);
+
+      const { name, email, gender, address, street, city, phone } = data;
+      dispatch({
+        type: FETCH_DATA_SUCCESS,
+        payload: data.dataObject,
+      });
+    } catch (error: Error | any) {
+      dispatch({
+        type: FETCH_DATA_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+  };
 
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, fetchAllData }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
